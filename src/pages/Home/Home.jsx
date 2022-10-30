5c71277fb7541e2d5a4ba9c7fa93c2559c058a4e
Lucas Mendes <lucasespassini@gmail.com>
2022-10-30 16:35:34 -0300

    tratando erro

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,6 +12,7 @@ const spin = keyframes`
 export const Home = () => {
   const [dados, setDados] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [deuErro, setDeuErro] = useState(false)
   const [recarregar, setRecarregar] = useState(false)
   const prefersReducedMotion = usePrefersReducedMotion()
 
@@ -22,41 +23,51 @@ export const Home = () => {
   useEffect(() => {
     (async () => {
       setIsLoading(true)
-      const { data } = await axios.get('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
-      setDados(data)
-      setIsLoading(false)
-      setRecarregar(false)
-      console.log(data)
+      try {
+        const { data } = await axios.get('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
+        setDados(data)
+        setIsLoading(false)
+        setRecarregar(false)
+        setDeuErro(false)
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+        setDeuErro(true)
+      }
     })()
   }, [recarregar])
 
   return (
-    <Container h="60vh">
-      <Text my={7} align="center" fontWeight="600">
-        Última atualização {dados.hg}
-      </Text>
-      {dados?.cand?.map(candidato => (
-        <Canditado
-          key={candidato.seq}
-          nome={candidato.nm}
-          num={candidato.n}
-          partido={candidato.cc.split(' ')[0]}
-          porc={candidato.pvap}
-          votos={candidato.vap}
-          sqcand={candidato.sqcand}
-        />
-      ))}
-      <IconButton
-        colorScheme="facebook"
-        size="lg"
-        position="absolute"
-        bottom={2}
-        right={5}
-        animation={isLoading && animation}
-        onClick={() => setRecarregar(true)}
-        borderRadius={999}
-        icon={<RepeatIcon />}
-      />
-    </Container>
+    <>
+      {deuErro && !isLoading
+        ? <Text>Ocorreu um erro</Text>
+        : <Container h="60vh">
+          <Text my={7} align="center" fontWeight="600">
+            Última atualização {dados.hg}
+          </Text>
+          {dados?.cand?.map(candidato => (
+            <Canditado
+              key={candidato.seq}
+              nome={candidato.nm}
+              num={candidato.n}
+              partido={candidato.cc.split(' ')[0]}
+              porc={candidato.pvap}
+              votos={candidato.vap}
+              sqcand={candidato.sqcand}
+            />
+          ))}
+          <IconButton
+            colorScheme="facebook"
+            size="lg"
+            position="absolute"
+            bottom={2}
+            right={5}
+            animation={isLoading && animation}
+            onClick={() => setRecarregar(true)}
+            borderRadius={999}
+            icon={<RepeatIcon />}
+          />
+        </Container>}
+    </>
   )
 }
